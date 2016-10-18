d318b805216a72379f42e9b93f528c5eb7c207d3
Molly Henderson <molly.henderson@crosschx.com>
2016-10-18 01:39:49 -0400

    This is awkward

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function () {
