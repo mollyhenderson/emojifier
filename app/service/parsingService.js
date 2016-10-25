6ad32d10416968fe05d7e0ca82e48939a6206715
Molly Henderson <molly.henderson@crosschx.com>
2016-10-25 01:03:05 -0400

    That was easy!

--- app/service/parsingService.js
+++ app/service/parsingService.js
@@ -5,7 +5,6 @@ const constants = require('../util/constants');
 const HttpError = require('../util/httpError');
 
 function parseSlackMessage(body) {
-  console.log(body);
   // sample body.text: 'emojify <url> as <name>'
   //   or: 'aliasify <existing_emoji> as <name>'
   let messageParts = body.text.split(' ');
